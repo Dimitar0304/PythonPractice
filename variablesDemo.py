45b76252b8c6bc51ee35d4cc8b49b2e2f8a4a858
Димитър Банчев <114139864+Dimitar0304@users.noreply.github.com>
2024-12-03 10:09:33 +0200

    practice

---
+++ variablesDemo.py
@@ -0,0 +1,8 @@
+first = input("first number ") #10
+second = input("second number ") #20
+print(first + second) #input 1020
+
+#if we want to print  str + int we will throw
+
+print(int(first) + int(second))
+
